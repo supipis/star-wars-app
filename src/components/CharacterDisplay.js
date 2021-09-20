a46b1946833a09f5a4a5ab05a04a34866fda6ddf
supipis <70693604+supipis@users.noreply.github.com>
2021-09-20 11:37:03 +0200

    fetched info from the planets

--- src/components/CharacterDisplay.js
+++ src/components/CharacterDisplay.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const CharacterDisplay = ({ item }) => {
+
+	const [homeworld, setHomeworld] = useState("Loading...");
+	const [loading, setLoading] = useState(false);
+
+
+	useEffect(() => {
+		const fetchHomeworld = async () => {
+			setLoading(true);
+			let response = await axios(item.homeworld)
+			//console.log(response.data.name)
+			setHomeworld(response.data.name)
+			setLoading(false)
+		}
+		fetchHomeworld()
+	}, [])
+
 	return (
 		<div className="item-details">
 			<h1>{item.name}</h1>
@@ -8,7 +25,7 @@ const CharacterDisplay = ({ item }) => {
 				<li>Birth year :{item.birth_year}</li>
 				<li>Gender : {item.gender}</li>
 				<li>height : {item.height}</li>
-				<li>Home world : {item.homeworld}</li>
+				<li>Home world : {homeworld}</li>
 
 			</ul>
 		</div>
